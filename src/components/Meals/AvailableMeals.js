6937b9761519be6852b9438150ca143ccf1ab5f5
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-07-07 14:07:22 +0200

    Adding items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -29,25 +29,24 @@ const DUMMY_MEALS = [
   },
 ];
 
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) =>  (
+  const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
-      />
+    />
   ));
 
-    return (
-      <section className={classes.meals}>
-        <Card>
-          <ul> {mealsList}</ul>
-        </Card>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-   );
+  );
 };
 
 export default AvailableMeals;
